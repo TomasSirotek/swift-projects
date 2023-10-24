a1ec7374ad9d0fac98c405e09849a75fd614d102
Tomas Simko <72190589+TomassSimko@users.noreply.github.com>
2023-10-24 21:18:00 +0200

    inital project

---
+++ weather-app/weather-app/weather_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  weather_appApp.swift
+//  weather-app
+//
+//  Created by simko on 24/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct weather_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
