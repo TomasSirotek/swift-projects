3be885499480364dcaad5bcc70798f8627ed2944
Tomas Simko <72190589+TomassSimko@users.noreply.github.com>
2023-10-27 01:43:36 +0200

    fm initial

---
+++ framework/framework/frameworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  frameworkApp.swift
+//  framework
+//
+//  Created by Tomas Sirotek on 27/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct frameworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
