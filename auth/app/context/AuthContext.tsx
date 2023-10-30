4e79c276bb0b1a9eb90690e2379d31ed60ea2441
Tomas Simko <72190589+TomassSimko@users.noreply.github.com>
2023-10-30 02:06:09 +0100

    auth base context

---
+++ auth/app/context/AuthContext.tsx
@@ -0,0 +1,11 @@
+import {createContext,useContext,useEffect,useState} from 'react';
+import axios from 'axios';
+import * as SecureStore from 'expo-secure-store';
+
+
+interface AuthContextProps{
+  authState?: {token: string | null, authenticated: boolean | null},
+  onRegister?: (email: string, password: string) => Promise<any>,
+  onLogin?: (email: string, password: string) => Promise<any>,
+  onLogout?: () => Promise<any>,
+}
